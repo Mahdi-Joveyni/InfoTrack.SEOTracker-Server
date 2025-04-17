2dfc9b09dbd8a4a81fad1a1957ddac90b0febc7f
Mahdi Joveyni <91252320+Mahdi-Joveyni@users.noreply.github.com>
2025-04-17 09:13:58 +0100

    fix data test performance (#3)

--- test/InfoTrack.SEOTracker.Data.Tests/BaseTest.cs
+++ test/InfoTrack.SEOTracker.Data.Tests/BaseTest.cs
@@ -1,12 +1,18 @@
 ﻿using Mongo2Go;
 
 namespace InfoTrack.SEOTracker.Data.Tests;
-public abstract class BaseTest : IClassFixture<MongoDbFixture>
+public abstract class BaseTest : IClassFixture<MongoDbFixture>, IDisposable
 {
    protected readonly MongoDbRunner _runner;
    protected BaseTest(MongoDbFixture fixture)
    {
-      fixture.InitializeAsync().GetAwaiter().GetResult();
+      fixture.Initialize();
       _runner = fixture.Runner!;
+   }
+
+   public void Dispose()
+   {
+      _runner.Dispose();
+      GC.SuppressFinalize(this);
    }
 }
